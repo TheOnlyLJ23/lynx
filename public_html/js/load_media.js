bd5d9e62b3d0129e6fdcef330e6f7b9c2c6cf9d9
Liam <liamjames1960@hotmail.it>
2020-08-22 17:17:15 +0200

    building server side

---
+++ public_html/js/load_media.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public_html', 'index1.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('send media', () => {
+    console.log('button clicked');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
